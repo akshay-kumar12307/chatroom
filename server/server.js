f96bad951dfb7bf7c22704530690c0e51bcd6290
akshay-kumar12307 <akshaykumartammisetti@gmail.com>
2025-12-31 06:54:55 +0530

    Render deployment ready

--- server/server.js
+++ server/server.js
@@ -91,7 +91,9 @@ io.on("connection", socket => {
   });
 
 });
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () =>
-  console.log("✅ Server running at http://localhost:3000")
-);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
